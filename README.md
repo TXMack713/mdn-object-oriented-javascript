c810b2b8a505fee73c0d005d3251e0980d7c9ff9
TXMack713 <mackattack713@gmail.com>
2021-08-02 23:06:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mdn-object-oriented-javascript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mdn-object-oriented-javascript)
---
+++ script.js
@@ -0,0 +1,87 @@
+function Person(first, last, age, gender, ...interests) {
+  this.name = {
+    first: first,
+    last: last
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+  this.answer = '';
+  if (this.interests.length > 2) {
+    for (let i = 0; i < this.interests.length; i++) {
+      if (i <= this.interests.length - 2) {
+        this.answer += this.interests[i] + ', ';
+      } else if (i === this.interests.length - 1) {
+        this.answer += ' and ' + this.interests[i];
+      }
+    }
+  } else if (this.interests.length == 2) {
+    this.answer += interests[0] + ' and ' + interests[1];
+  } else if (this.interests.length === 1) {
+    this.answer = interests[0];
+  }
+
+  if (this.gender.toLowerCase() === 'm') {
+    if (this.interests.length === 0) {
+      this.bio = function() {
+        alert(
+          this.name.first +
+            ' ' +
+            this.name.last +
+            ' is ' +
+            this.age +
+            ' years old.'
+        );
+      };
+    } else {
+      if (this.interests.length > 0) {
+        this.bio = function() {
+          alert(
+            this.name.first +
+              ' ' +
+              this.name.last +
+              ' is ' +
+              this.age +
+              ' years old. He likes ' +
+              this.answer +
+              '.'
+          );
+        };
+      }
+    }
+  } else {
+    if (this.interests.length === 0) {
+      this.bio = function() {
+        alert(
+          this.name.first +
+            ' ' +
+            this.name.last +
+            ' is ' +
+            this.age +
+            ' years old.'
+        );
+      };
+    } else {
+      if (this.interests.length > 0) {
+        this.bio = function() {
+          alert(
+            this.name.first +
+              ' ' +
+              this.name.last +
+              ' is ' +
+              this.age +
+              ' years old. She likes ' +
+              this.answer +
+              '.'
+          );
+        };
+      }
+    }
+  }
+
+  this.greeting = function() {
+    alert("Hi! I'm " + this.name.first + '.');
+  };
+}
+
+let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
